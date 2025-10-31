bf30e73be0bb922918e16e91d76063fd46041259
Patrick Nguyen <patrick.nguyen2011@gmail.com>
2025-10-31 17:47:21 +0100

    chore: KnowMore component updated

--- site/src/app/components/know-more/KnowMore.tsx
+++ site/src/app/components/know-more/KnowMore.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss';
 import cn from 'classnames';
 import { ReactNode } from 'react';
+import { Heading } from '@/app/components/heading/Heading';
 
 type KnowMoreProps = {
   variant: 'purple' | 'yellow';
@@ -15,11 +16,9 @@ type KnowMoreProps = {
 export default function KnowMore({ variant, knowMore, children, titleAs = 'h2' }: KnowMoreProps) {
   return (
     <div className={cn([styles.container, styles[`container--${variant}`]])}>
-      {titleAs === 'h2' ? (
-        <h2 className={cn('fr-text--md', styles['container__title'])}>{knowMore?.title}</h2>
-      ) : (
-        <h3 className={cn('fr-text--md fr-h2', styles['container__title'])}>{knowMore?.title}</h3>
-      )}
+      <Heading headingLevel={titleAs} className={cn('fr-text--md', styles['container__title'])}>
+        <>{knowMore?.title}</>
+      </Heading>
       <p className="fr-text--md fr-mb-0 fr-mt-1w">{knowMore?.description}</p>
       {children && <div className="fr-mt-1w">{children}</div>}
     </div>
