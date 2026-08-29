644262976762898c3374a680329d926ee595b48e
Patrick Nguyen <patrick.nguyen2011@gmail.com>
2026-08-29 15:01:34 +0200

    chore: updated site map (#540)

--- site/src/app/v2/plan-du-site/page.tsx
+++ site/src/app/v2/plan-du-site/page.tsx
@@ -3,8 +3,6 @@ import PageTitle from '../../../../components/PageTitle/PageTitle';
 import styles from './style.module.scss';
 import Link from 'next/link';
 import { SKIP_LINKS_ID } from '@/app/constants/skip-links';
-import { CHATBOT_EXTERNAL_URL, CHATBOT_EXTERNAL_URL_TITLE } from '@/app/constants/urls';
-import { shouldDisplayChatbot } from '@/utils/date';
 
 export default function PlanDuSite() {
   return (
@@ -29,18 +27,11 @@ export default function PlanDuSite() {
               <Link href="/v2/structures">Structures sportives</Link>
             </li>
             <li>
-              <Link href="/v2/partenaires">Partenaires</Link>
-            </li>
-            <li>
               <Link href="/v2/trouver-un-club">Trouver un club partenaire</Link>
             </li>
             <li>
               <Link href="/v2/une-question">Une question ?</Link>
             </li>
-            {/* todo: enable later */}
-            {/*<li>*/}
-            {/*  <Link href="/v2/budget">Budget</Link>*/}
-            {/*</li>*/}
             <li>
               <Link href="/v2/mentions-legales">Mentions légales</Link>
             </li>
@@ -48,14 +39,6 @@ export default function PlanDuSite() {
               <Link href="/v2/politique-de-confidentialite">Données personnelles</Link>
             </li>
           </ul>
-
-          {shouldDisplayChatbot() && (
-            <p>
-              <Link href={CHATBOT_EXTERNAL_URL} target="_blank">
-                {CHATBOT_EXTERNAL_URL_TITLE}
-              </Link>
-            </p>
-          )}
         </section>
       </main>
 
